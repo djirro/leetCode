dfd54d3cf93b72c9ea09e80f9618eefb28afcc7e
Djirro <djirrochannel@gmail.com>
2022-04-11 00:02:03 +0300

    Создал репозиторий, и дополнительную ветку. Решил первую задачу под номером 13

---
+++ 13. Roman to Integer/RomanToInteger.playground/Contents.swift
@@ -0,0 +1,15 @@
+class Solution {
+    private let romanDict: [Character: Int] = ["I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000]
+    
+    func romanToInt(_ s: String) -> Int {
+        var prev = 0, out = 0
+        
+        for index in s {
+            let romanNumber = romanDict[index] ?? 0
+            out += romanNumber <= prev ? prev : -prev
+            prev = romanNumber
+        }
+        out += prev
+        return out
+    }
+}
