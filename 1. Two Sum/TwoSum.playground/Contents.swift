3804ac87c0b25fa98c51223f4c031ca4d2d5905b
Djirro <djirrochannel@gmail.com>
2022-04-13 22:43:54 +0300

    тестовый коммит

--- 1. Two Sum/TwoSum.playground/Contents.swift
+++ 1. Two Sum/TwoSum.playground/Contents.swift
@@ -1,3 +1,12 @@
-import UIKit
+let target = 7
+let nums = [1,2,5,6,4,3]
+let sortedNums = nums.sorted()
+let sortedTargetNums = sortedNums[]
 
-var greeting = "Hello, playground"
+//for i in sortedNums.count {
+//    print(sortedNums[i])
+//    // guard sortedNums[index] <= target else {
+//    //     print("guard")
+//    //     return
+//    // }
+//}
